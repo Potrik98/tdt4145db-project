74113fc5f597fcccbb6719fe8ea3fe9e094d2788
potrik98 <potrik98@gmail.com>
2018-03-17 21:22:43 +0100

    Add google guava dependency and ordering for exercises

--- src/main/java/com/roervik/tdt4145/dbproject/model/Exercise.java
+++ src/main/java/com/roervik/tdt4145/dbproject/model/Exercise.java
@@ -1,13 +1,17 @@
 package com.roervik.tdt4145.dbproject.model;
 
+import com.google.common.collect.Ordering;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.Comparator;
 import java.util.UUID;
 
 @Data
 @Builder(toBuilder = true)
 public class Exercise {
+    public static final Ordering<Exercise> ordering = Ordering.from(Comparator.comparing(Exercise::getExerciseId));
+
     final UUID exerciseId;
     final String name;
     final String description;
--- src/main/java/com/roervik/tdt4145/dbproject/model/ExerciseWithEquipment.java
+++ src/main/java/com/roervik/tdt4145/dbproject/model/ExerciseWithEquipment.java
@@ -1,13 +1,17 @@
 package com.roervik.tdt4145.dbproject.model;
 
+import com.google.common.collect.Ordering;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.Comparator;
 import java.util.UUID;
 
 @Data
 @Builder(toBuilder = true)
 public class ExerciseWithEquipment {
+    public static final Ordering<ExerciseWithEquipment> ordering = Ordering.from(Comparator.comparing(ExerciseWithEquipment::getExerciseId));
+
     final UUID exerciseId;
     final String description;
     final Equipment equipment;
