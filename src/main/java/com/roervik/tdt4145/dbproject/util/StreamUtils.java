6b1f9d71d9840bfba46c03ed39fab551ca2cc00a
potrik98 <potrik98@gmail.com>
2018-03-17 20:35:28 +0100

    Add uncheckRunnable to stream utils

--- src/main/java/com/roervik/tdt4145/dbproject/util/StreamUtils.java
+++ src/main/java/com/roervik/tdt4145/dbproject/util/StreamUtils.java
@@ -8,4 +8,10 @@ public class StreamUtils {
         catch (RuntimeException e) { throw e; }
         catch (Exception e) { throw new RuntimeException(e); }
     }
+
+    public static void uncheckRun(RunnableExc r) {
+        try { r.run(); } catch (Exception e) { throw new RuntimeException(e); }
+    }
+
+    public interface RunnableExc { void run() throws Exception; }
 }
